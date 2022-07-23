44dda643aee45e0ef777dd91ff16e07337f9e625
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-24 02:54:40 +0300

    Массив из 8 элементов

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+void FillArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        col[i] = new Random().Next(1, 50);
+    }
+}
+
+void PrintArray(int [] col)
+{
+    for (int position = 0; position < col.Length; position++)
+    {
+        Console.Write($"{col[position]}, ");
+    }
+}
+
+int [] array = new int[8];
+FillArray(array);
+PrintArray(array);
