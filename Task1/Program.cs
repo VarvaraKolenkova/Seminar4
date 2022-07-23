715e1d7c7a848351363a7404edd98316e4524a29
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-24 01:32:50 +0300

    Число А в степени В

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Цикл, который принимает на вход 2 числа (А и В) и возводит число А в степень В
+Console.WriteLine("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+
+int result = a;
+for (int i = 2; i <= b; i++)
+{
+    result*=a;
+}
+Console.WriteLine($"{a} в степени {b} = {result}");
